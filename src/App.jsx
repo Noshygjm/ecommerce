8f27c79c73fa7c66febea1273020cbd2e40b26b2
Noshygjm <moresi.guillermo@gmail.com>
2024-10-16 13:58:59 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import './App.css'
-import Button from './components/Button/Button'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import TituloH1 from './components/Titulos/TituloH1'
@@ -13,9 +11,13 @@ function App() {
 
       <NavBar/>
 
+      <main>
+
       <TituloH1 h1="Mil y una formas de aprender" />
 
       <ItemListContainer mensaje="Bienvenidos a tu proxima capacitación"/>
+      
+      </main>
 
     </>
   )
